f9b0c88bf9cad64f109351ce27e0f182ea911fac
Eddie Dias <eddias.mazor@gmail.com>
2024-06-19 06:19:25 -0300

    Finalização do projeto

--- core/configs.py
+++ core/configs.py
@@ -1,16 +1,23 @@
+import os
 from typing import ClassVar
+
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
+
 
 class Settings(BaseSettings):
     """
     Settings class for the FastAPI app
     """
     API_V1_STR: str = "/api/v1"
-    DB_URL: str = "postgresql+asyncpg://postgres:rootxvk9@localhost:5432/fastapi"
+    DB_URL: str = os.getenv("DB_URL")
     DBBaseModel: ClassVar = declarative_base()
 
     class Config:
         case_sensitive = True
+
 
 settings = Settings()
